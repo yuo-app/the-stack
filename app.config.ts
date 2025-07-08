05787f99d44f4bcc550f07a86958f3366f69cd07
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2025-07-08 10:45:25 +0200

    fix: what

--- app.config.ts
+++ app.config.ts
@@ -43,6 +43,11 @@ export default defineConfig({
       ? undefined
       : {
           deployConfig: true,
+          wrangler: {
+            observability: {
+              enabled: true,
+            },
+          },
         },
   },
 })
